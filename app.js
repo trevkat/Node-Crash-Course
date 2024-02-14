9e7543e37e569fbe37ea66e789676d6ac9b39d11
Trevor Katilius <111081073+trevkat@users.noreply.github.com>
2024-02-14 16:08:46 -0600

    Creating new views for express app

--- app.js
+++ app.js
@@ -16,16 +16,14 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-    //res.send('<p>About Page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about');
 });
 
-// redirect
-app.get('/about-us', (req, res) => {
-    res.redirect('./about');
-});
+app.get('/blogs/create'), (req, res) => {
+    res.render();
+};
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404');
 });
