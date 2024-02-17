79868a7812b7bf1321c6638da15bdad68828cee6
Trevor Katilius <111081073+trevkat@users.noreply.github.com>
2024-02-16 20:56:46 -0600

    Updates to app.js and tested post request

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // Compare this to the old code in server_part_one, two, & three. This is more elegant and easier to use with less code. 
 
 const express = require('express');
+const bodyParser = require('body-parser')
 
 // express app
 const app = express();
+
+// create application/json parser
+var jsonParser = bodyParser.json()
 
 // register view engine
 app.set('view engine', 'ejs');
@@ -23,7 +27,21 @@ app.get('/blogs/create', (req, res) => {
     res.render('create');
 });
 
+app.get('/person', (req, res) => {
+    res.json({
+       name: 'paddy'
+    });
+});
+
+app.post('/person', jsonParser, (req, res) => {
+    console.log(req.body.name)
+    res.json({
+       name: req.body.name
+    });
+});
+
 // 404 page
 app.use((req, res) => {
     res.status(404).render('404');
 });
+
