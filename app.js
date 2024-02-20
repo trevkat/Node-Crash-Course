d42ccea42357c7bd6640cb660a23281e7b1a7fe1
Trevor Katilius <111081073+trevkat@users.noreply.github.com>
2024-02-20 10:28:23 -0600

    using middleware in node.js

--- app.js
+++ app.js
@@ -15,6 +15,20 @@ app.set('view engine', 'ejs');
 // listen for requests
 app.listen(3000);
 
+// next function allows the code to proceed here. Page will hang and fail to load without next
+app.use((req, res, next) => {
+    console.log('new request made:');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('in the next middleware');
+    next();
+});
+
 // passing data into views by passing { title: 'Home' }
 app.get('/', (req, res) => {
     const blogs = [
@@ -33,7 +47,7 @@ app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create' });
 });
 
-// 404 page
+// 404 page - has to be at the bottom since requests run from top to bottom 
 app.use((req, res) => {
     res.status(404).render('404', { title: '404' });
 });
