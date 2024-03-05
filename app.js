7711496798ba19829a23a03fce997e3aac31f3a4
Trevor Katilius <111081073+trevkat@users.noreply.github.com>
2024-03-05 16:12:44 -0600

    updated page to include a POST request when creating a new blog

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // mongoose and mongo sandbox routes
@@ -102,6 +103,17 @@ app.get('/blogs', (req, res) => {
     })
 })
 
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+    .then((result) => {
+        res.redirect('/blogs');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create' });
 });
